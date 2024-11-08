aed04e648fb6e5b90d767c76d4d20eb1f03969f7
foxiinc <kirill20061231@gmail.com>
2024-11-08 23:16:31 +0700

    messagepizdec

--- perfume_store/shop/urls.py
+++ perfume_store/shop/urls.py
@@ -2,15 +2,10 @@ from django.urls import path
 from .views import ProductListAPIView, ProductDetailAPIView, CartListAPIView, CartDetailAPIView, OrderListAPIView, OrderDetailAPIView
 
 urlpatterns = [
-    # Продукты
-    path('products', ProductListAPIView.as_view(), name='product-list-create'),
-    path('products/<int:pk>', ProductDetailAPIView.as_view(), name='product-detail'),
-
-    # Корзины
-    path('carts', CartListAPIView.as_view(), name='cart-list-create'),
-    path('carts/<int:pk>', CartDetailAPIView.as_view(), name='cart-detail'),
-
-    # Заказы
-    path('orders', OrderListAPIView.as_view(), name='order-list-create'),
-    path('orders/<int:pk>', OrderDetailAPIView.as_view(), name='order-detail'),
+    path('api/products/', ProductListAPIView.as_view(), name='product-list-create'),
+    path('api/products/<int:pk>/', ProductDetailAPIView.as_view(), name='product-detail'),
+    path('api/cart/', CartListAPIView.as_view(), name='cart-list-create'),
+    path('api/cart/<int:pk>/', CartDetailAPIView.as_view(), name='cart-detail'),
+    path('api/orders/', OrderListAPIView.as_view(), name='order-list-create'),
+    path('api/orders/<int:pk>/', OrderDetailAPIView.as_view(), name='order-detail'),
 ]
