bd905f4d5e720180d8d866cb2fd00b1359e737e7
foxiinc <kirill20061231@gmail.com>
2024-11-08 23:37:44 +0700

    messagepizdec

--- perfume_store/shop/urls.py
+++ perfume_store/shop/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import ProductListAPIView, ProductDetailAPIView, CartListAPIView, CartDetailAPIView, OrderListAPIView, OrderDetailAPIView
 
 urlpatterns = [
-    path('api/products/', ProductListAPIView.as_view(), name='product-list-create'),
+    path('api/products', ProductListAPIView.as_view(), name='product-list-create'),
     path('api/products/<int:pk>/', ProductDetailAPIView.as_view(), name='product-detail'),
-    path('api/cart/', CartListAPIView.as_view(), name='cart-list-create'),
+    path('api/carts', CartListAPIView.as_view(), name='cart-list-create'),
     path('api/cart/<int:pk>/', CartDetailAPIView.as_view(), name='cart-detail'),
-    path('api/orders/', OrderListAPIView.as_view(), name='order-list-create'),
+    path('api/orders', OrderListAPIView.as_view(), name='order-list-create'),
     path('api/orders/<int:pk>/', OrderDetailAPIView.as_view(), name='order-detail'),
 ]
